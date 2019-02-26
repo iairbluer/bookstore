8dc93a54193695e2a6cab20e0b7a3287649e9338
DESKTOP-JGHD1EA\Iair Bluer <iairbluer@gmail.com>
2019-02-26 11:27:28 +0200

    1. added cloud db using atlas and added mongodb uri to heroku

--- config/env/production.js
+++ config/env/production.js
@@ -13,7 +13,7 @@ module.exports = {
   // Binding to 127.0.0.1 is safer in production.
   host: process.env.HOST || '0.0.0.0',
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://totangoUser:totangoPassword>@cluster0-shard-00-00-op5dl.mongodb.net:27017,cluster0-shard-00-01-op5dl.mongodb.net:27017,cluster0-shard-00-02-op5dl.mongodb.net:27017/bookStoreDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',
     options: {
       /**
       * Uncomment to enable ssl certificate based authentication to mongodb

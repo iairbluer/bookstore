16d721f9c2ad8c019e6627473fe9f3481165dc58
DESKTOP-JGHD1EA\Iair Bluer <iairbluer@gmail.com>
2019-03-01 18:24:12 +0200

    1, added semantic ui 2. added new loader 3. added new look for book list

--- config/assets/default.js
+++ config/assets/default.js
@@ -9,7 +9,8 @@ module.exports = {
         // bower:css
         'public/lib/bootstrap/dist/css/bootstrap.css',
         'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-        'public/lib/angular-ui-notification/dist/angular-ui-notification.css'
+        'public/lib/angular-ui-notification/dist/angular-ui-notification.css',
+        'public/lib/semantic/semantic.min.css'
         // endbower
       ],
       js: [
@@ -24,6 +25,7 @@ module.exports = {
         'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/semantic/semantic.min.js'
         // endbower
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
